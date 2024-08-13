71556bcd3b9ca056f240bc49c35a709fd51dbdc6
rayzinnz <raymond.gabriel@gmail.com>
2024-08-13 18:30:06 +1200

    added list files_in_prefix and unittests

---
+++ run.py
@@ -0,0 +1,22 @@
+﻿"""Running the Xetra ETL application"""
+import logging
+import logging.config
+
+import yaml
+
+def main():
+    """
+    entry point to run the xetra etl job
+    """
+    # Parsing YAML file
+    config_path = r"C:\Users\hrag\Sync\Study\ETL\xetra_project\xetra_rayzinnz_1234\configs\xetra_report1_config.yml"
+    config = yaml.safe_load(open(config_path))
+    # print(config)
+    # configure logging
+    log_config = config['logging']
+    logging.config.dictConfig(log_config)
+    logger = logging.getLogger(__name__)
+    logger.info("This is a test.")
+
+if __name__ == '__main__':
+    main()
--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -1,5 +1,6 @@
 ﻿"""Connector and methods accessing S3"""
 import os
+import logging
 
 import boto3
 
@@ -16,14 +17,24 @@ class S3BucketConnector():
         :param endpoint_url: endpoint url to S3
         :param bucket: S3 bucket name
         """
+        self._logger = logging.getLogger(__name__)
         self.endpoint_url = endpoint_url
         self.session = boto3.Session(aws_access_key_id=os.environ[access_key],
                                     aws_secret_access_key=os.environ[secret_key])
         self._s3 = self.session.resource(service_name='s3', endpoint_url=self.endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
-    
-    def list_files_in_prefix(self):
-        pass
+
+    def list_files_in_prefix(self, prefix: str):
+        """
+        listing all files with a prefix on the S3 bucket
+        
+        :param pregix: prefix on the S3 bucket that should be filtered with
+
+        returns:
+            files: list of all the file names containing the prefix in the key
+        """
+        files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
+        return files
 
     def read_csv_to_df(self):
         pass
